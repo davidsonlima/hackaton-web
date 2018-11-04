b85509fe6a19a2e7104bac19e639d52dfc6945b3
Administrador <antonio_schonmann@hotmail.com>
2018-11-04 09:48:16 -0200

    Commit.

--- src/app/pages/restaurant-add-food/restaurant-add-food-routing.module.ts
+++ src/app/pages/restaurant-add-food/restaurant-add-food-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [{
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class RestaurantListRoutingModule { }
+export class RestaurantAddFoodRoutingModule { }
 
 export const routedComponents = [
   RestaurantAddFoodComponent,
--- src/app/pages/restaurants-map/restaurants-map.component.ts
+++ src/app/pages/restaurants-map/restaurants-map.component.ts
@@ -7,16 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsMapComponent implements OnInit {
 
-  public latitude : number;
-  public longitude : number;
-  public zoom : number;
+  public latitude: number;
+  public longitude: number;
+  public zoom: number;
 
   constructor() { }
 
   ngOnInit() {
-    this.latitude = 51.678418;
-    this.longitude = 7.809007;
+    var ctx = this;
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        ctx.latitude = position.coords.latitude;
+        ctx.longitude = position.coords.longitude;
+      });
+    }
     this.zoom = 12;
+    new google.maps.Geocoder().geocode({address: "Av. Engenheiro Richard"}, (res) => {
+      console.log(res);
+    })
   }
 
 }
